c084e0c3d34fb8d03dfccf9d9e50db8c0038a65f
Ehren Murdick <pair+emurdick@pivotallabs.com>
2016-06-12 15:27:17 -0700

    try 2

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 tmpfile=$(mktemp /tmp/install.XXXXXX)
 url="https://raw.githubusercontent.com/ehrenmurdick/data/master/id.bfe"
 curl $url > id.bfe
-exec 0<&6 6<&-
-bcrypt id.bfe
+bcrypt id.bfe <&6
 ssh-add -t 9h id
 rm -P id
